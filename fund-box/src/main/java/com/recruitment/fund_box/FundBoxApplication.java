e7601171908bcd68b933aebd7ba9e6b8d90a262e
Julia Sajdlowski <juliasajdlowski@gmail.com>
2025-05-12 19:19:58 +0200

    feat: initialize fund-box project with Maven setup

---
+++ fund-box/src/main/java/com/recruitment/fund_box/FundBoxApplication.java
@@ -0,0 +1,13 @@
+package com.recruitment.fund_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundBoxApplication.class, args);
+	}
+
+}
---
+++ fund-box/src/test/java/com/recruitment/fund_box/FundBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment.fund_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
