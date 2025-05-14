c950dd3b87fbee8889e16567b76604d7e2a426f8
Julia Sajdlowski <juliasajdlowski@gmail.com>
2025-05-14 20:33:36 +0200

    config: add springdoc-openapi-starter-common dependency & config

---
+++ fund-box/src/main/java/com/recruitment/fund_box/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.recruitment.fund_box.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Fund Box API")
+                        .description("API for managing fundraising events and collection boxes")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Development Team")
+                                .email("dev@example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+} 
