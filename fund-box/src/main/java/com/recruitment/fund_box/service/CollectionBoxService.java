0d4523abc77e1ea99e409f19c0d314ea0fad614e
Julia Sajdlowski <juliasajdlowski@gmail.com>
2025-05-14 15:38:14 +0200

    feat: add donation and fund transfer methods, add financial report to FundraisingEventService

--- fund-box/src/main/java/com/recruitment/fund_box/service/CollectionBoxService.java
+++ fund-box/src/main/java/com/recruitment/fund_box/service/CollectionBoxService.java
@@ -2,7 +2,9 @@ package com.recruitment.fund_box.service;
 
 import com.recruitment.fund_box.dto.request.CollectionBoxRequest;
 import com.recruitment.fund_box.dto.response.CollectionBoxResponse;
+import com.recruitment.fund_box.enums.Currency;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface CollectionBoxService {
@@ -10,5 +12,6 @@ public interface CollectionBoxService {
     List<CollectionBoxResponse> getAllCollectionBoxes();
     void removeCollectionBox(Long boxId);
     void assignCollectionBox(Long boxId, Long eventId);
-
+    void makeDonation(Long id, Currency currency, BigDecimal amount);
+    void transferFunds(Long boxId);
 }
--- fund-box/src/main/java/com/recruitment/fund_box/service/FundraisingEventService.java
+++ fund-box/src/main/java/com/recruitment/fund_box/service/FundraisingEventService.java
@@ -2,9 +2,12 @@ package com.recruitment.fund_box.service;
 
 import com.recruitment.fund_box.dto.request.FundraisingEventRequest;
 import com.recruitment.fund_box.dto.response.FundraisingEventResponse;
+import com.recruitment.fund_box.dto.response.FinancialReportResponse;
 
+import java.util.List;
 
 public interface FundraisingEventService {
     FundraisingEventResponse createFundraisingEvent(FundraisingEventRequest fundraisingEventRequest);
     FundraisingEventResponse getFundraisingEvent(Long eventId);
+    List<FinancialReportResponse> getFinancialReport();
 }
