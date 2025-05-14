04ac6a298010a722248d6f47c4604e911fc41308
Julia Sajdlowski <juliasajdlowski@gmail.com>
2025-05-14 15:39:09 +0200

    feat: implement getFinancialReport method in FundraisingEventServiceImpl

--- fund-box/src/main/java/com/recruitment/fund_box/service/FundraisingEventServiceImpl.java
+++ fund-box/src/main/java/com/recruitment/fund_box/service/FundraisingEventServiceImpl.java
@@ -3,10 +3,14 @@ package com.recruitment.fund_box.service;
 import com.recruitment.fund_box.domain.FundraisingEvent;
 import com.recruitment.fund_box.dto.request.FundraisingEventRequest;
 import com.recruitment.fund_box.dto.response.FundraisingEventResponse;
+import com.recruitment.fund_box.dto.response.FinancialReportResponse;
 import com.recruitment.fund_box.exceptions.ResourceNotFoundException;
 import com.recruitment.fund_box.mappers.FundraisingEventMapper;
 import com.recruitment.fund_box.repository.FundraisingEventRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FundraisingEventServiceImpl implements FundraisingEventService{
@@ -33,4 +37,14 @@ public class FundraisingEventServiceImpl implements FundraisingEventService{
         return fundraisingEventMapper.eventToEventDto(event);
     }
 
+    @Override
+    public List<FinancialReportResponse> getFinancialReport() {
+        return fundraisingEventRepository.findAll().stream()
+                .map(event -> new FinancialReportResponse(
+                    event.getName(),
+                    event.getFunds(),
+                    event.getAccountCurrency()
+                ))
+                .collect(Collectors.toList());
+    }
 }
